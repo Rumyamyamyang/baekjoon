aea7dfd3a3dfea603bcaaa8e125d7f321907fe74
Rumya <rumia0528@naver.com>
2024-07-20 22:44:24 +0900

    Random Marathon G

---
+++ 28xxx/28062.cpp
@@ -0,0 +1,38 @@
+// https://x.com/rumya_study/status/1814148671367516479
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	int bundle{ 0 }, input{ 0 };
+	std::vector<int> candy;
+	int sum{ 0 };
+
+	std::cin >> bundle;
+
+	for (int i = 0; i < bundle; ++i)
+	{
+		std::cin >> input;
+		sum += input;
+		candy.push_back(input);
+	}
+
+	std::sort(candy.begin(), candy.end());
+
+	for (int i = 0; i < bundle; ++i)
+	{
+		if (sum % 2 == 0)
+		{
+			break;
+		}
+
+		if (candy[i] % 2 == 1)
+		{
+			sum -= candy[i];
+		}
+	}
+
+	std::cout << sum;
+}
