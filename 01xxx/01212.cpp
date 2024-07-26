d18d0182cdcf47a292943ff169d474d924566de6
Rumya <rumia0528@naver.com>
2024-07-26 23:46:49 +0900

    Random Marathon 8 / D, E

---
+++ 01xxx/01212.cpp
@@ -0,0 +1,52 @@
+// https://x.com/rumya_study/status/1816845243200979144
+
+#define TEST false
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+int main()
+{
+	std::string octal, binary{ "" }, temp{ "" };
+	int target{ 0 };
+
+	std::cin >> octal;
+
+	for (int i = 0; i < octal.size(); ++i)
+	{
+		target = octal[i] - '0';
+
+		while (target != 0)
+		{
+			temp = std::to_string(target % 2) + temp;
+			target /= 2;
+		}
+
+		while (temp.size() != 3)
+		{
+			temp = '0' + temp;
+		}
+
+		binary += temp;
+
+#if TEST == true
+		binary += "/";
+#endif
+
+		temp = { "" };
+	}
+
+	while (binary.front() == '0')
+	{
+		// num is 0
+		if (binary.size() == 1)
+		{
+			break;
+		}
+
+		binary.erase(binary.begin());
+	}
+
+	std::cout << binary;
+}
---
+++ 30xxx/30045.cpp
@@ -0,0 +1,48 @@
+// https://x.com/rumya_study/status/1816827339185226208
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	int sentenceAmount{ 0 }, output{ 0 };
+	std::string input;
+	std::vector<std::string> emoticon{ "01" , "OI" };
+	bool isExist{ false };
+
+	std::cin >> sentenceAmount;
+
+	for (int i = 0; i < sentenceAmount; ++i)
+	{
+		std::cin >> input;
+
+		// find including string
+		for (int j = 0; j < emoticon.size(); ++j)
+		{
+			while (true)
+			{
+				size_t index = input.find(emoticon[j]);
+
+				if (index == std::string::npos || isExist == true)
+				{
+					break;
+				}
+
+				input.replace(index, emoticon[j].size(), "!");
+				isExist = true;
+			}
+		}
+
+		if (isExist == true)
+		{
+			++output;
+			isExist = false;
+		}
+	}
+
+	std::cout << output;
+
+	return 0;
+}
